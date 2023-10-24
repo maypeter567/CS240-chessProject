d21e5ee6c91449b8d77c7040bbb0c5f9dcc3c736
Peter <maypeter0090@gmail.com>
2023-10-24 11:45:22 -0600

    AuthDAO.java flushed out for implementation.

--- src/main/data_access/AuthDAO.java
+++ src/main/data_access/AuthDAO.java
@@ -3,7 +3,14 @@ package data_access;
 import dataAccess.DataAccessException;
 import models.AuthTokenMod;
 
+import java.util.Map;
+import java.util.Objects;
+
 public class AuthDAO {
+    
+    int i = 0;
+    
+    Map<String, String> tokens;
     
     /**
      * returns an authentication token from the database.
@@ -12,7 +19,11 @@ public class AuthDAO {
      * @return a AuthToken for future communications with the database.
      * @throws DataAccessException if the authorization request is denied
      */
-    public final AuthTokenMod getAuthToken(String username) throws DataAccessException { return null; }
+    public final AuthTokenMod getAuthToken(String username) throws DataAccessException {
+        String AuthToken = AuthTokenGenerator();
+        tokens.put(AuthToken, username);
+        return new AuthTokenMod(AuthToken, username);
+    }
     
     /**
      * checks to see if the token is still valid.
@@ -21,5 +32,13 @@ public class AuthDAO {
      * @return returns whether the token is still valid.
      * @throws DataAccessException if token doesn't exist in database
      */
-    public final boolean checkVerified(AuthTokenMod token) throws DataAccessException { return false; }
+    public final boolean checkVerified(AuthTokenMod token) throws DataAccessException {
+        return Objects.equals(tokens.get(token.getAuthToken()), token.getUsername());
+    }
+    
+    private String AuthTokenGenerator() {
+        String toReturn = String.valueOf(i);
+        i++;
+        return toReturn;
+    }
 }
