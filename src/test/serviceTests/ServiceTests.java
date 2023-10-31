11fb4279d36d9d35a40b0116e89f001b89e1d1ee
Peter <maypeter0090@gmail.com>
2023-10-31 11:46:50 -0600

    making code easier to test

--- src/test/serviceTests/ServiceTests.java
+++ src/test/serviceTests/ServiceTests.java
@@ -2,6 +2,7 @@ package serviceTests;
 
 import dataAccess.DataAccessException;
 import data_access.*;
+import jdk.jfr.Registered;
 import models.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -140,7 +141,7 @@ class ServiceTests {
         
         CreateGameResult testResult;
         try {
-            testResult = new CreateGame().createGame(testRequest, testToken, stack);
+            testResult = new CreateGame().createGame(testRequest, new AuthTokenMod("frog", "frog"), stack);
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
         }
@@ -152,15 +153,17 @@ class ServiceTests {
             throw new RuntimeException(e);
         }
         
-        Assertions.assertEquals(allGames.keySet().toArray()[0], 700*45);
+        Assertions.assertEquals(stack.lastElement(), 401);
     }
     
     @Test
     public void joinTestPass() {
         JoinRequest joinRequest = new JoinRequest();
+        Stack<Integer> stack = new Stack<Integer>();
         
+        int testGameID;
         try {
-            int testGameID = gameDAO.createNewGame("testGame");
+            testGameID = gameDAO.createNewGame("testGame");
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
         }
@@ -173,7 +176,223 @@ class ServiceTests {
         }
         
         joinRequest.setAuthToken(testToken);
+        joinRequest.setGameID(testGameID);
+        joinRequest.setPlayerColor("WHITE");
         
+        JoinResult joinResult;
+        try {
+            joinResult = new Join().join(joinRequest, testToken, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         
+        Assertions.assertEquals(stack.lastElement(), 200);
+    }
+    
+    @Test
+    public void joinTestFail() {
+        JoinRequest joinRequest = new JoinRequest();
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        int testGameID;
+        try {
+            testGameID = gameDAO.createNewGame("testGame");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        AuthTokenMod testToken = null;
+        try {
+            testToken = authDAO.getAuthToken("test1");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        joinRequest.setAuthToken(testToken);
+        joinRequest.setGameID(testGameID+4001);
+        joinRequest.setPlayerColor("WHITE");
+        
+        JoinResult joinResult;
+        try {
+            joinResult = new Join().join(joinRequest, testToken, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 400);
+    }
+    
+    @Test
+    public void listGamesPass() {
+        JoinRequest joinRequest = new JoinRequest();
+        Stack<Integer> stack = new Stack<>();
+        
+        int testGameID;
+        try {
+            testGameID = gameDAO.createNewGame("testGame");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        AuthTokenMod testToken = null;
+        try {
+            testToken = authDAO.getAuthToken("test1");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        ListGamesResult listGamesResult;
+        try {
+            listGamesResult = new ListGames().listGames(testToken, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 200);
+    }
+    
+    @Test
+    public void listGamesFail() {
+        JoinRequest joinRequest = new JoinRequest();
+        Stack<Integer> stack = new Stack<>();
+        
+        int testGameID;
+        try {
+            testGameID = gameDAO.createNewGame("testGame");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        AuthTokenMod testToken = null;
+        try {
+            testToken = authDAO.getAuthToken("test1");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        ListGamesResult listGamesResult;
+        try {
+            listGamesResult = new ListGames().listGames(new AuthTokenMod("frog", "frog"), stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 401);
+    }
+    
+    @Test
+    public void loginResultPass() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("test1");
+        loginRequest.setPassword("pass");
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        LoginResult loginResult;
+        try {
+            loginResult = new Login().login(loginRequest, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 200);
+    }
+    
+    @Test
+    public void loginResultFail() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("test1");
+        loginRequest.setPassword("password");
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        LoginResult loginResult;
+        try {
+            loginResult = new Login().login(loginRequest, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 401);
+    }
+    
+    @Test
+    public void logoutPass() {
+        LogoutRequest logoutRequest = new LogoutRequest();
+        Stack<Integer> stack = new Stack<>();
+        
+        AuthTokenMod testToken = null;
+        try {
+            testToken = authDAO.getAuthToken("test1");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        LogoutResult logoutResult;
+        try {
+            logoutResult = new Logout().logout(logoutRequest, testToken, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 200);
+    }
+    
+    @Test
+    public void logoutFail() {
+        LogoutRequest logoutRequest = new LogoutRequest();
+        Stack<Integer> stack = new Stack<>();
+        
+        AuthTokenMod testToken = null;
+        try {
+            testToken = authDAO.getAuthToken("test1");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        LogoutResult logoutResult;
+        try {
+            logoutResult = new Logout().logout(logoutRequest, new AuthTokenMod("froggy", "pall"), stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 401);
+    }
+    
+    @Test
+    public void registerPass() {
+        RegisterRequest registerRequest = new RegisterRequest();
+        registerRequest.setPassword("pass");
+        registerRequest.setUsername("test11");
+        registerRequest.setEmail("email");
+        Stack<Integer> stack = new Stack<>();
+        
+        RegisterResult registerResult;
+        try {
+            registerResult = new Register().register(registerRequest, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 200);
+    }
+    
+    @Test
+    public void registerFail() {
+        RegisterRequest registerRequest = new RegisterRequest();
+        registerRequest.setPassword("password");
+        registerRequest.setUsername("test1");
+        registerRequest.setEmail("email");
+        Stack<Integer> stack = new Stack<>();
+        
+        RegisterResult registerResult;
+        try {
+            registerResult = new Register().register(registerRequest, stack);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        Assertions.assertEquals(stack.lastElement(), 403);
     }
 }
